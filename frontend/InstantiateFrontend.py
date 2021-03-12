665b64bc6e202a243c2e315e9be2c459cfd53749
tesinclair <66572748+tesinclair@users.noreply.github.com>
2021-03-12 18:28:26 +0000

    Chnages to InstantiateForntend Login Setup

--- frontend/InstantiateFrontend.py
+++ frontend/InstantiateFrontend.py
@@ -1,2 +1,40 @@
-def run():
-    pass
+from frontend import Box
+import pygame
+
+def setup_login(screen, dim: Tuple):
+    # Constant Values
+
+    BOX_SPACING = 10
+
+    # Gets Dimensions
+
+    height = dim[0]
+    width = dim[1]
+
+    # Creates Login text
+
+
+
+    # Username Box
+
+    username_box_login = Box.EnglishInputBox(
+        screen, 
+        (height/4, width/6), 
+        (30,60), 
+        text="Username..."
+        )
+
+    # Password Box
+
+    password_box_login = Box.EnglishInputBox(
+        screen, 
+        (height/4 + username_box_login.get_dim[0] + BOX_SPACING, 100), 
+        (30,60), 
+        text="Password...",
+        type="password"
+        )
+    
+    # Returns the instances
+
+    return username_box_login, password_box_login
+    
